9110d704a5311962a226dbc504650577f0e78519
bhavin027 <bhavinghoghari27@gmail.com>
2020-07-31 19:30:58 +0530

    created user and cart sagas for sign in, sign up amd sign in with google

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ import SignInSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.compon
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './component/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
+
+import { checkUserSession } from './redux/user/user.actions';
 
 
 class App extends React.Component {
@@ -23,22 +23,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-  	const { setCurrentUser } = this.props;
+  	const { checkUserSession } = this.props;
 
-  	this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-  		if (userAuth) {
-  			const userRef = await createUserProfileDocument(userAuth);
-
-  			userRef.onSnapshot(snapShot => {
-  				setCurrentUser({
-					id: snapShot.id,
-					...snapShot.data()
-  				});
-  			});
-  		}
-  		setCurrentUser(userAuth);
-		//addCollectionAndDocuments('collections', collectionsArray.map( ({ title, items }) => ({ title, items })  ));
-  	});
+  	checkUserSession();
   }
 
   componentWillUnmount() {
@@ -67,7 +54,7 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-	setCurrentUser: user =>dispatch(setCurrentUser(user))
+	checkUserSession: () => dispatch(checkUserSession())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
